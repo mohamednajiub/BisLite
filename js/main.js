ed52dda77f5e44983d0d7ae8e20eb36aad0a6d6c
mohamednajiub <m.emad550@gmail.com>
2019-09-06 17:05:57 +0200

    fix latest works UI

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-stage').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:4
+            items:2
         },
         1000:{
+            items:3
+        },
+        1200: {
             items:4
         }
     },
     mouseDrag: true,
     dots: false,
     freeDrag: true
+});
+$('.owl-prev').click(function() {
+    $('.owl-stage').trigger('prev.owl.carousel');
+})
+// Go to the previous item
+$('.owl-next').click(function() {
+    $('.owl-stage').trigger('next.owl.carousel', [300]);
 })
